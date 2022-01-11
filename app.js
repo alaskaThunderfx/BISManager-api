39f5a2a50f302cfb4d77bab7a37dc3e5bb1c54f2
alaskaThunderfx <scott.thomas.mackey@gmail.com>
2022-01-11 12:24:28 -0500

    Added useNewUrlParser, useCreateIndex, useUnifiedTopology while continuing to troubleshoot the .env issues

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-require('dotenv/config')
-const port = process.env.PORT
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv/config");
+const port = process.env.PORT;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // import routes
-const userRoute = require('./routes/user')
-const gearSetRoute = require('./routes/gearset')
+const userRoute = require("./routes/user");
+const gearSetRoute = require("./routes/gearset");
 
-app.use('/users', userRoute)
-app.use('/gearsets', gearSetRoute)
+app.use("/users", userRoute);
+app.use("/gearsets", gearSetRoute);
 
 //ROUTES
-app.get('/', (req, res) => {
-    res.send('We are on home!')
-    console.log(typeof process.env.DB_CONNECT)
-})
+app.get("/", (req, res) => {
+  res.send("We are on home!");
+  console.log(typeof process.env.DB_CONNECT);
+});
 
 // connect to DB
-mongoose.connect(process.env.DB_CONNECT, () => {
-    console.log('Connected to DB!')
-})
+mongoose.connect(process.env.DB_CONNECT, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(port, () => {
-    console.log("listening on port" + port)
-})
+  console.log("listening on port" + port);
+});
